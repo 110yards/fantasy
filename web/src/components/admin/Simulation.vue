746c7704d04d2fb6bb9f0c2f093af13bcff4f2a1
Michael Dryden <mjdryden@gmail.com>
2021-12-12 11:23:17 -0600

    TODOs for admin

---
+++ web/src/components/admin/Simulation.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-row>
+    TODO: Update games (full week, up to game / quarter)
+    <br />
+    TODO: End of Day
+    <br />
+    TODO: End of Week
+  </v-row>
+</template>
+
+<script>
+export default {}
+</script>
--- web/src/views/admin/Admin.vue
+++ web/src/views/admin/Admin.vue
@@ -6,12 +6,20 @@
       <v-btn class="caption" text @click="setView('status')">Status</v-btn>
       <v-btn class="caption" text @click="setView('problems')">Problems</v-btn>
       <v-btn class="caption" text @click="setView('leagues')">Leagues</v-btn>
+      <v-btn class="caption" text @click="setView('maintenance')">Maintenance</v-btn>
+      <v-btn class="caption" text @click="setView('sim')">Simulation</v-btn>
     </v-card-text>
 
     <v-card-text>
       <status v-if="view == 'status'" />
       <problems v-if="view == 'problems'" />
       <leagues v-if="view == 'leagues'" />
+      <div v-if="view == 'maintenance'">
+        TODO: Change year<br />
+        TODO: End the year (stop jobs, change state)<br />
+        TODO: Start the year (start jobs, change state, fetch list of games)<br />
+      </div>
+      <simulation v-if="view == 'sim'" />
     </v-card-text>
   </v-card>
 </template>
@@ -20,8 +28,9 @@
 import Problems from "../../components/admin/Problems.vue"
 import Status from "../../components/admin/Status.vue"
 import Leagues from "../../components/admin/Leagues.vue"
+import Simulation from "../../components/admin/Simulation.vue"
 export default {
-  components: { Status, Problems, Leagues },
+  components: { Status, Problems, Leagues, Simulation },
   name: "Admin",
   data() {
     return {
