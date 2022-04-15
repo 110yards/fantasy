7823b27736fc86b1621774440f9766d54be4592f
Michael Dryden <mjdryden@gmail.com>
2022-04-15 14:59:02 -0500

    Cleanup

--- api/tests/mocks/mock_notification_service.py
+++ api/tests/mocks/mock_notification_service.py
@@ -1,17 +1,4 @@
-from fastapi.param_functions import Depends
 from api.app.domain.entities.league import League
-from api.app.domain.repositories.league_config_repository import LeagueConfigRepository, create_league_config_repository
-from api.app.domain.services.discord_service import DiscordService, create_discord_service
-
-
-def create_notification_service(
-    config_repo: LeagueConfigRepository = Depends(create_league_config_repository),
-    discord_service: DiscordService = Depends(create_discord_service),
-):
-    return NotificationService(
-        config_repo=config_repo,
-        discord_service=discord_service,
-    )
 
 
 class MockNotificationService:
