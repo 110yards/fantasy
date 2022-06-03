45623ef3923ea4b0b71434b3c588daf2d2f1bee4
Michael Dryden <mjdryden@gmail.com>
2022-06-03 15:10:31 -0500

    broken import

--- services/system/app/routers/migration_router.py
+++ services/system/app/routers/migration_router.py
@@ -3,16 +3,13 @@
 from services.system.app.domain.migrations.recreate_league_cmd_sub import LeagueCommandSubMigration, create_league_cmd_sub_migration
 from typing import Optional
 from starlette.requests import Request
-from api.app.core.role import Role
-from api.app.core.auth import require_role
 from fastapi.params import Depends
-from api.app.routers.api_router import APIRouter
+from .api_router import APIRouter
 
 router = APIRouter(prefix="/migration")
 
 
 @router.post("/recreate_league_cmd_sub")
-@require_role(Role.admin)
 async def recreate_league_cmd_sub(
     request: Request,
     league_id: Optional[str] = None,
