9985b402bfbb70b88affb204eb00e4d8a77634ce
Michael Dryden <mjdryden@gmail.com>
2022-06-09 16:20:53 -0500

    Better invalid email handling

--- services/api/app/domain/commands/user/register_email.py
+++ services/api/app/domain/commands/user/register_email.py
@@ -8,6 +8,7 @@ from fastapi import Depends
 from yards_py.core.annotate_args import annotate_args
 from yards_py.core.base_command_executor import BaseCommand, BaseCommandResult, BaseCommandExecutor
 from firebase_admin import auth
+from firebase_admin.exceptions import InvalidArgumentError
 from firebase_admin.auth import UserRecord
 from yards_py.core.logging import Logger
 from yards_py.core.publisher import Publisher
@@ -51,13 +52,13 @@ class RegisterCommandExecutor(BaseCommandExecutor[RegisterEmailCommand, Register
             result = auth.create_user(display_name=command.display_name, email=command.email)  # type: UserRecord
         except auth.EmailAlreadyExistsError:
             return RegisterEmailResult(command=command, error="A user with that email already exists")
-        except ValueError as ex:
-            message = ex.args[0] if hasattr(ex, "args") and len(ex.args) > 0 else "Unable to create user account"
+        except InvalidArgumentError as ex:
+            message = ex.args[0] if hasattr(ex, "args") and len(ex.args) > 0 else "Unable to create user account (validation error)"
             Logger.warn(message, exc_info=ex)
             return RegisterEmailResult(command=command, error=message)
         except Exception as ex:
             Logger.error("Create user call failed", exc_info=ex)
-            return RegisterEmailResult(command=command, error="Unable to create user account")
+            return RegisterEmailResult(command=command, error="Unable to create user account (unknown error)")
 
         if self.is_dev:
             # In live, this is handled by a background cloud function triggered after the registration
