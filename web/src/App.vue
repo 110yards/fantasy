9f7ed3fbc21be301ee08569be8491c6644598517
Michael Dryden <mjdryden@gmail.com>
2022-11-18 21:12:23 -0600

    Formatting

--- web/src/App.vue
+++ web/src/App.vue
@@ -23,13 +23,7 @@
       </v-container>
     </v-main>
     <snack-bar ref="snackBar" />
-    <a
-      style="display:none"
-      rel="me"
-      href="https://fosstodon.org/@mdryden"
-    >
-      @mdryden
-    </a>
+    <a style="display: none" rel="me" href="https://fosstodon.org/@mdryden"> @mdryden </a>
   </v-app>
 </template>
 
