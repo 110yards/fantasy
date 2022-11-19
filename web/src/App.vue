07e6404f93d3b0993c9f764f998caff94f3c3b10
Michael Dryden <5611490+mdryden@users.noreply.github.com>
2022-11-18 21:01:34 -0600

    Update App.vue

--- web/src/App.vue
+++ web/src/App.vue
@@ -23,8 +23,13 @@
       </v-container>
     </v-main>
     <snack-bar ref="snackBar" />
-    <a style="display:none" rel="me"
-      href="https://fosstodon.org/@mdryden">@mdryden</a>
+    <a
+      style="display:none"
+      rel="me"
+      href="https://fosstodon.org/@mdryden"
+    >
+      @mdryden
+    </a>
   </v-app>
 </template>
 
